d1ccb5dc89b3cf72436a295aeba381382d33db55
mertcan <dikmenmertcan@gmail.com>
2025-07-26 20:35:53 +0300

    first commit

---
+++ src/test/java/tests/Jenkins.java
@@ -0,0 +1,49 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class Jenkins {
+
+    @Test
+    void Test1(){
+        assertTrue(true);
+    }
+
+    private void assertTrue(boolean b) {
+    }
+
+    @Test
+    void Test2(){
+        assertTrue(true);
+    }
+    @Test
+    void Test3(){
+        assertTrue(true);
+    }
+    @Test
+    void Test4(){
+        assertTrue(true);
+    }
+    @Test
+    void Test5(){
+        assertTrue(true);
+    }
+    @Test
+    void Test6(){assertTrue(true);}
+    
+    @Test
+    void Test7(){ assertTrue(true);}
+    
+    @Test
+    void Test8(){
+        assertTrue(true);
+    }
+    @Test
+    void Test9(){
+        assertTrue(true);
+    }
+    @Test
+    void Test10(){
+        assertTrue(true);
+    }
+}
